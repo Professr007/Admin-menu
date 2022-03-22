f1c83166d0a7e51b0f264515820fdf6bdf11b6a2
LudiYoutuber65 <67953605+LudiYoutuber65@users.noreply.github.com>
2022-03-22 14:04:57 +0100

    Add files via upload
    Dodani logovi

---
+++ config.lua
@@ -0,0 +1,2 @@
+Config                            = {}
+Config.Webhuk 					  = 'https://discord.com/api/webhooks/955804152099926036/cx8r_lbhPfFyU5tvhKGQoaiyL0kda3mnwbG5vn0HgF7CK3NhWQs1zoqzk7dx-Y8OQqRU'
